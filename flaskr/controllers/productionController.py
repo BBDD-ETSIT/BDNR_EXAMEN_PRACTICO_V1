c0c1018fdbffd04b3962cef24b68384f928cedab
anmunoz <andres.max36@gmail.com>
2025-01-10 13:31:22 +0100

    First commit

---
+++ flaskr/controllers/productionController.py
@@ -0,0 +1,30 @@
+from models import Production
+from sparql import query
+from flask import g, request
+from mongoengine import Q
+
+
+def read_productions():
+     #Completar la función
+    print("Reading movies...")
+    productions = ""
+    
+    
+    return productions
+
+def read_productions_by_filter(min_number_of_awards, max_number_of_awards ):
+    #Completar la función para filtar por rango de valores de galardones
+    productions = ""
+
+    
+    return productions
+
+
+def read_production(alias):
+    print("Reading Production...", alias)
+    #Completar la función
+    production = ""
+    
+    return production
+
+
